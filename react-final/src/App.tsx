d9dfa8a0a3eaf15715174567bdd02accb68d0cf8
Nguyen Tai Duc <dleviathan1989@gmail.com>
2020-06-02 23:38:13 +0700

    change npm to yarn

--- react-final/src/App.tsx
+++ react-final/src/App.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+import {ListCoin} from "./components/home/home-page";
+
 function App() {
     return (
         <Router>
+            <div>
+                <ul>
+                    <li>
+                        <Link to={ListCoin}>Home</Link>
+                    </li>
+                    <li>
+                        <Link to='/coin/:id'>Detail</Link>
+                    </li>
+                </ul>
+            </div>
 
+            <Route path='/coin/:id' exact component={}/>
         </Router>
     );
 }
